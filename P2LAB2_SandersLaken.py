65164192e08c98830604a15c1a7fc38ca9d715bc
sandersl9910 <113523790+sandersl9910@users.noreply.github.com>
2022-10-24 08:40:34 -0400

    Add files via upload

---
+++ P2LAB2_SandersLaken.py
@@ -0,0 +1,10 @@
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+
+avg = (num1 + num2 + num3 + num4) / 4
+product = num1 * num2 * num3 * num4
+
+print(f'{product:.0f} {avg:.0f}')
+print(f'{product:.3f} {avg:.3f}')
